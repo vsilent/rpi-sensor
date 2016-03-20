1665561ad7de0e74343d58fe2d471614f4b3ceb9
vsilent <jabberroid@gmail.com>
2016-03-20 13:42:41 +0200

    sensor code fixes

--- hcsro04.py
+++ hcsro04.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import time
+
 GPIO.setmode(GPIO.BCM)
 
 #set GPIO Pins
@@ -25,21 +26,21 @@ def distance():
         StartTime = time.time()
 
         # save time of arrival
-        while GPIO.input(GPIO_ECHO) == 1:
-            StopTime = time.time()
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
 
-            # time difference between start and arrival
-            TimeElapsed = StopTime - RPiStartTime
-            # multiply with the sonic speed (34300 cm/s)
-            # and divide BOARDy 2, because there and back
-            distance = (TimeElapsed * 34300) / 2
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide BOARDy 2, because there and back
+    distance = (TimeElapsed * 34300) / 2
 
-            return distance
+    return distance
 
 if __name__ == '__main__':
     try:
         while True:
-            dist = setmodedistance()
+            dist = distance()
             print ("Measured Distance = %.1f cm" % dist)
             time.sleepp(1)
 
