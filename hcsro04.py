bd8c11ca11cdf3bfc8934f266c7047fed9616a0b
vsilent <jabberroid@gmail.com>
2016-03-20 13:30:23 +0200

    sensor code updated

--- hcsro04.py
+++ hcsro04.py
@@ -2,29 +2,48 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 
-TRIG = 23
-ECHO = 24
+#set GPIO Pins
+GPIO_TRIGGER = 23
+GPIO_ECHO = 24
 
-print "Distance Measurement In Progress"
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
 
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
+def distance():
+    # set TRIGrigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
 
-GPIO.output(TRIG, False)
-print "Waiting For Sensor To Settle"
-time.sleep(2)
+    StartTime = time.time()
+    StopTime = time.time()
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
 
-while GPIO.input(ECHO)==0:
-      pulse_start = time.time()
+        # save time of arrival
+        while GPIO.input(GPIO_ECHO) == 1:
+            StopTime = time.time()
 
-      while GPIO.input(ECHO)==1:
-            pulse_end = time.time()
-            pulse_duration = pulse_end - pulse_start
-            distance = pulse_duration * 17150
-            distance = round(distance, 2)
-            print "Distance:",distance,"cm"
-            GPIO.cleanup()
+            # time difference between start and arrival
+            TimeElapsed = StopTime - RPiStartTime
+            # multiply with the sonic speed (34300 cm/s)
+            # and divide BOARDy 2, because there and back
+            distance = (TimeElapsed * 34300) / 2
+
+            return distance
+
+if __name__ == '__main__':
+    try:
+        while True:
+            dist = setmodedistance()
+            print ("Measured Distance = %.1f cm" % dist)
+            time.sleepp(1)
+
+    # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
